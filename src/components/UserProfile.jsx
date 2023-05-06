964bdf84a77583b784120e4d183f79c87bf3c8f0
Regis066 <mugisharegis72@gmail.com>
2023-05-06 22:37:06 +0200

    added new feature

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -46,6 +46,25 @@ const UserProfile = () => {
   alt='user-pic'
   />
   <h1 className='font-bold text-3xl text-center mt-3'> {user.username} </h1>
+  <div className="absolute top-0 z-1 right-0 p-2">
+  {userId === user.id && (
+    <GoogleLogout  
+    clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+    render={(renderProps) => (
+      <button
+      type='button'
+      className='bg-white p-2 rounded-full cursor-pointer outline-none shadow-md'
+      onClick={renderProps.onClick}
+      disabled={renderProps.disabled}
+      >
+      <AiOutlineLogout color='red' fontSize={21}  />
+      </button>
+    )}
+    onLogoutSuccess={Logout}
+    cookiePolicy={'single_host_origin'}
+    />
+  )}
+  </div>
   </div>
   </div>
   </div>
