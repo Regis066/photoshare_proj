83e3439a446e47a8853913fa3a5d772b1984d15a
Regis066 <mugisharegis72@gmail.com>
2023-05-07 17:23:55 +0200

    added new thing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,19 @@ import { gapi } from 'gapi-script'
 import {Routes, Route , useNavigate} from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+   const user = fetchUser();
+
+   if(!user){
+    navigate('/login');
+   }
+  }, [])
+  
 
   useEffect(()=>{
     function start (){
